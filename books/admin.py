aab3e5a31013e7eaa982e101c7df7e6ebb42ef3f
ShuaybAi <aijaz.shuayb@gmail.com>
2025-09-29 22:59:15 +0100

    add models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser, Book, Review, Comment
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Comment)
