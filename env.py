d84fa14c92c109fa4c46407e47e1266e782df0b2
ShuaybAi <aijaz.shuayb@gmail.com>
2025-09-29 14:21:39 +0100

    add to gitignore file

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault('SECRET_KEY', 'django-insecure-i!c71e88a)h#falqp89@e+9(a)g81^l-#e3eyl(-+$mp4i_gti')
+os.environ.setdefault('SECRET_KEY', '~){8W+5M{<y[j4nIz+*U@w,u!nilU]_G1I)O7G{#"_VX4D$)M9/zamjvV[HUK2{')
 # os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_qTypG3fQW8dZ@ep-calm-unit-agdt9qtj.c-2.eu-central-1.aws.neon.tech/fog_cleft_putt_641057')
 # os.environ.setdefault('CLOUDINARY_URL', 'cloudinary://145792327648878:fgnv3IdgxYFFBpsrATGwu3pIDFI@diioj4vn5')
 os.environ.setdefault('DEVELOPMENT', '1')
