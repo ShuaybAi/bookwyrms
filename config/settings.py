fcc0c379416a8f285ddbc05b097589d5181f0de5
ShuaybAi <aijaz.shuayb@gmail.com>
2025-09-30 10:43:29 +0100

    add whitenoise to requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# WhiteNoise static file serving
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
