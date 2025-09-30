4e54949a61dce9b4c0d08f5d2dd81672be6f0814
ShuaybAi <aijaz.shuayb@gmail.com>
2025-09-30 02:14:38 +0100

    install django-summernote

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,10 @@
 """Admin configuration for the books app."""
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import CustomUser, Book, Review, Comment
 
 # Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Book)
-admin.site.register(Review)
-admin.site.register(Comment)
+admin.site.register(CustomUser, SummernoteModelAdmin)
+admin.site.register(Book, SummernoteModelAdmin)
+admin.site.register(Review, SummernoteModelAdmin)
+admin.site.register(Comment, SummernoteModelAdmin)
