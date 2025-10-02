8dc1244e2ee6c04c432581ac3385f01f9361af5c
ShuaybAi <aijaz.shuayb@gmail.com>
2025-10-02 14:18:09 +0100

    add add_book page

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('add-book/', views.add_book, name='add_book'),
 ]
